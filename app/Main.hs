452f781a7d43c47d2ae1ae001b5e3f5e731e982f
Jamshid <qq@qq.com>
2017-04-29 07:56:39 -0700

    First commit for a lightweight Webdav server written in Haskell.

---
+++ app/Main.hs
@@ -0,0 +1,286 @@
+{-# LANGUAGE
+      DataKinds,
+      FlexibleInstances,
+      MultiParamTypeClasses,
+      OverloadedStrings,
+      RecordWildCards,
+      TypeOperators
+#-}
+
+module Main where
+
+import qualified Data.ByteString.Lazy.Char8 as Lazy.Char8
+import Data.DateTime
+
+import Servant
+import Servant.API
+import Servant.API.ContentTypes
+import Servant.Foreign.Internal
+
+import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.AddHeaders
+import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Servant.Options
+
+import Text.XML.Light
+import Text.XML.Light.Output
+import Text.XML.Light.Types
+
+
+data DavMethod = MKCOL | PROPFIND | PROPPATCH | LOCK | UNLOCK | ORDERPATCH | COPY | MOVE
+
+
+-- OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, COPY, MOVE
+
+instance ReflectMethod 'MKCOL where
+  reflectMethod _ = "MKCOL"
+instance ReflectMethod 'PROPFIND where
+  reflectMethod _ = "PROPFIND"
+instance ReflectMethod 'PROPPATCH where
+  reflectMethod _ = "PROPPATCH"
+instance ReflectMethod 'LOCK where
+  reflectMethod _ = "LOCK"
+instance ReflectMethod 'UNLOCK where
+  reflectMethod _ = "UNLOCK"
+instance ReflectMethod 'ORDERPATCH where
+  reflectMethod _ = "ORDERPATCH"
+--instance ReflectMethod 'HEAD where
+--  reflectMethod _ = "HEAD"
+--instance ReflectMethod 'TRACE where
+--  reflectMethod _ = "TRACE"
+instance ReflectMethod 'COPY where
+  reflectMethod _ = "COPY"
+instance ReflectMethod 'MOVE where
+  reflectMethod _ = "MOVE"
+
+
+
+type Mkcol = Verb 'MKCOL 200
+type Propfind = Verb 'PROPFIND 207
+type Proppatch = Verb 'PROPPATCH 200
+type Lock = Verb 'LOCK 200
+type Unlock = Verb 'UNLOCK 200
+type Orderpatch = Verb 'ORDERPATCH 200
+--type Head = Verb 'HEAD 200
+--type Trace = Verb 'TRACE 200
+type Copy = Verb 'COPY 200
+type Move = Verb 'MOVE 200
+
+data XML = XML
+
+e name attrs content = Element{elName=QName{qName=name,qURI=Nothing,qPrefix=Just "D"}, elAttribs=attrs, elContent = map Elem content, elLine=Nothing}
+te name attrs text = Element{elName=QName{qName=name,qURI=Nothing,qPrefix=Just "D"}, elAttribs=attrs, elContent = [Text $ CData CDataText text Nothing], elLine=Nothing}
+
+instance MimeRender XML [Int] where
+  mimeRender proxy items =
+    Lazy.Char8.pack $ showTopElement $ 
+    e "multistatus"
+    [Attr (unqual "xmlns:D") "DAV:"]
+    [folder "/", folder "/aFolder", file "/aFile"]
+
+instance MimeRender XML [FSObject] where
+  mimeRender proxy items =
+    Lazy.Char8.pack $ showTopElement $ 
+    e "multistatus"
+    [Attr (unqual "xmlns:D") "DAV:"]
+    $ map fsObjectToXml items
+
+
+folder url = 
+  e "response" [] [
+    te "href" [] $ "http://127.0.0.1:20001" ++ url,
+    e "propstat" [] [
+      te "status" [] "HTTP/1.1 200 OK",
+      e "prop" [] [
+        e "resourcetype" [] [
+           e "collection" [] []
+           ],
+        te "getcontentlength" [] "10"]
+      ]
+    ]
+
+file url = 
+  e "response" [] [
+    te "href" [] $ "http://127.0.0.1:20001" ++ url,
+    e "propstat" [] [
+      te "status" [] "HTTP/1.1 200 OK",
+      e "prop" [] [
+        te "creationdate" [] "2017-04-27T08:33:10Z",
+        te "displayname" [] "aFile",
+        te "getlastmodified" [] "Thu, 27 Apr 2017 08:33:10 GMT",
+        e "resourcetype" [] [],
+        te "getcontentlength" [] "10"
+        ]
+      ]
+    ]
+
+data FSObject =
+  File {
+    displayName::String,
+    creationDate::DateTime,
+    lastModified::DateTime,
+    --resourcetype::??
+    getcontentlength::Int
+    }
+  | Folder {
+    displayName::String,
+    resourceType::String,
+    contentLength::Int
+    }
+
+getFileObject::FilePath->FSObject
+getFileObject path =
+  File {
+    displayName=path,
+    creationDate=fromSeconds 0,
+    lastModified=fromSeconds 0,
+    --resourcetype::??
+    getcontentlength=10
+    }
+
+--fsObjectToXml::FSObject->
+fsObjectToXml File{..} =
+  e "response" [] [
+    te "href" [] $ "http://127.0.0.1:20001/" ++ displayName,
+    e "propstat" [] [
+      te "status" [] "HTTP/1.1 200 OK",
+      e "prop" [] [
+        te "creationdate" [] "2017-04-27T08:33:10Z",
+        te "displayname" [] "aFile",
+        te "getlastmodified" [] "Thu, 27 Apr 2017 08:33:10 GMT",
+        e "resourcetype" [] [],
+        te "getcontentlength" [] "10"
+        ]
+      ]
+    ]
+fsObjectToXml Folder{..} =
+  e "response" [] [
+    te "href" [] $ "http://127.0.0.1:20001" ++ displayName,
+    e "propstat" [] [
+      te "status" [] "HTTP/1.1 200 OK",
+      e "prop" [] [
+        e "resourcetype" [] [
+           e "collection" [] []
+           ],
+        te "getcontentlength" [] "10"]
+      ]
+    ]
+
+getFolderObject::FilePath->FSObject
+getFolderObject folder =
+  Folder {
+    displayName=folder,
+    resourceType="",
+    contentLength=10
+    }
+
+
+
+
+  
+{-
+<?xml version="1.0" encoding="utf-8"?>
+<d:multistatus xmlns:d="DAV:">
+  <d:response>
+    <d:href>http://www.ajaxfilebrowser.com/</d:href>
+    <d:propstat>
+      <d:status>HTTP/1.1 200 OK</d:status>
+      <d:prop>
+        <d:creationdate>2014-02-26T01:53:16Z</d:creationdate>
+        <d:displayname>Storage</d:displayname>
+        <d:getlastmodified>Thu, 27 Apr 2017 08:33:10 GMT</d:getlastmodified>
+        <d:resourcetype><d:collection /></d:resourcetype>
+      </d:prop>
+    </d:propstat>
+    <d:propstat>
+      <d:status>HTTP/1.1 404 Not Found</d:status>
+      <d:prop>
+        <d:getcontentlength />
+        <d:getcontenttype />
+        <d:getetag />
+      </d:prop>
+      <d:responsedescription>Property was not found</d:responsedescription>
+    </d:propstat>
+  </d:response>
+  <d:response>
+    <d:href>http://www.ajaxfilebrowser.com/qqqq/</d:href>
+    <d:propstat>
+      <d:status>HTTP/1.1 200 OK</d:status>
+      <d:prop>
+        <d:creationdate>2017-04-27T08:33:10Z</d:creationdate>
+        <d:displayname>qqqq</d:displayname>
+        <d:getlastmodified>Thu, 27 Apr 2017 08:33:10 GMT</d:getlastmodified>
+        <d:resourcetype><d:collection /></d:resourcetype>
+      </d:prop>
+    </d:propstat>
+    <d:propstat>
+      <d:status>HTTP/1.1 404 Not Found</d:status>
+      <d:prop>
+        <d:getcontentlength />
+        <d:getcontenttype />
+        <d:getetag />
+      </d:prop>
+      <d:responsedescription>Property was not found</d:responsedescription>
+    </d:propstat>
+  </d:response>
+</d:multistatus>
+-}
+
+    
+instance Accept XML where
+  contentType _ = "text/xml"
+instance NotFound where
+  
+type UserAPI1 =
+  Mkcol '[JSON] [Int]
+  :<|> Propfind '[XML] [FSObject]
+  :<|> Proppatch '[JSON] [Int]
+  :<|> Lock '[JSON] [Int]
+  :<|> Unlock '[JSON] [Int]
+  :<|> Orderpatch '[JSON] [Int]
+  :<|> Get '[PlainText] String
+--  :<|> Head '[JSON] [Int]
+  :<|> Post '[JSON] [Int]
+  :<|> Put '[JSON] [Int]
+  :<|> Delete '[JSON] [Int]
+--  :<|> Trace '[JSON] [Int]
+  :<|> Copy '[JSON] [Int]
+  :<|> Move '[JSON] [Int]
+  :<|> "users" :> Propfind '[JSON] [Int]
+  :<|> "users" :> Get '[JSON] [Int]
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+app1 :: Application
+app1 = addHeaders [("Dav", "1, 2, ordered-collections")] $ provideOptions userAPI $ logStdoutDev $ serve userAPI
+       (
+         server1
+         :<|> doPropFind
+         :<|> server1
+         :<|> server1
+         :<|> server1
+         :<|> server1
+         :<|> server2
+         :<|> server1
+         :<|> server1
+         :<|> server1
+         :<|> server1
+         :<|> server1
+         :<|> server1
+         :<|> server1
+       )
+      
+main :: IO ()
+main = run 20001 app1
+
+
+--server1 :: Server UserAPI1
+server1 = return users1
+
+server2 = return "qq"
+
+doPropFind = return [getFolderObject "/", getFolderObject "/aFolder", getFileObject "http://127.0.0.1:20001/aFile"]
+
+users1::[Int]
+users1 = [1,2,3,4]
