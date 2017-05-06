e56141c08325ea7de2a9ce54e90e9a546c0e1f53
Jamshid <qq@qq.com>
2017-05-05 23:56:01 -0700

    Cleanup.

--- src/Network/WebDav/Server.hs
+++ src/Network/WebDav/Server.hs
@@ -4,6 +4,7 @@
 
 module Network.WebDav.Server where
 
+import Control.Monad
 import Control.Monad.IO.Class
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
@@ -40,79 +41,42 @@ webDavServer = addHeaders [("Dav", "1, 2, ordered-collections")] $ provideOption
 
 doMove::[String]->Maybe String->Handler ()
 doMove _ Nothing = error "Missing 'destination' header"
-doMove urlPath (Just destination) = do
-  let destination' = Char8.unpack (urlDecode False (Char8.pack destination))
-  liftIO $ putStrLn $ "In doMove: " ++ destination'
-  let fullPath = "/" ++ intercalate "/" urlPath
-
-  let relativePath =
-        if webBase `isPrefixOf` destination'
-        then drop (length webBase) destination'
-        else error "destination is not on this webdav server"
-  
-  liftIO $ putStrLn $ "Moving " ++ fullPath ++ " to " ++ relativePath
-
-  liftIO $ renamePath (fileBase ++ fullPath) (fileBase ++ relativePath)
+doMove urlPath (Just destination) = liftIO $ do
+  fromPath <- getSafePath urlPath
+  toPath <- getSafePath2 destination
+  renamePath fromPath toPath
 
 doCopy::[String]->Maybe String->Handler ()
 doCopy _ Nothing = error "Missing 'destination' header"
-doCopy urlPath (Just destination) = do
-  let destination' = Char8.unpack (urlDecode False (Char8.pack destination))
-  liftIO $ putStrLn $ "In doMove: " ++ destination'
-  let fullPath = "/" ++ intercalate "/" urlPath
-
-  let relativePath =
-        if webBase `isPrefixOf` destination'
-        then drop (length webBase) destination'
-        else error "destination is not on this webdav server"
-  
-  liftIO $ putStrLn $ "Moving " ++ fullPath ++ " to " ++ relativePath
-
-  liftIO $ copyFile (fileBase ++ fullPath) (fileBase ++ relativePath)
+doCopy urlPath (Just destination) = liftIO $ do
+  fromPath <- getSafePath urlPath
+  toPath <- getSafePath2 destination
+  copyFile fromPath toPath
 
 doPut::[String]->ByteString->Handler ()
-doPut urlPath body = do
-  liftIO $ putStrLn "In doPut"
-  let fullPath = "/" ++ intercalate "/" urlPath
-
-  liftIO $ putStrLn $ "Creating file: " ++ fullPath
-
-  liftIO $ ByteString.writeFile (fileBase ++ fullPath) body
+doPut urlPath body = liftIO $ flip ByteString.writeFile body =<< getSafePath urlPath
 
 doGet::[String]->Handler String
-doGet urlPath = do
-  liftIO $ putStrLn "In doGet"
-  let fullPath = "/" ++ intercalate "/" urlPath
-
-  liftIO $ putStrLn $ "Getting file: " ++ fullPath
-
-  liftIO $ readFile (fileBase ++ fullPath)
+doGet urlPath = liftIO $ readFile =<< getSafePath urlPath
 
 doDelete::[String]->Handler ()
-doDelete urlPath = do
-  liftIO $ putStrLn "In doDelete"
-  let fullPath = "/" ++ intercalate "/" urlPath
-  
-  liftIO $ removePathForcibly $ fileBase ++ fullPath
-
+doDelete urlPath = liftIO $ removePathForcibly =<< getSafePath urlPath
   
 doMkCol::[String]->Handler ()
-doMkCol urlPath = do
-  liftIO $ putStrLn "In doMkCol"
-  let fullPath = "/" ++ intercalate "/" urlPath
-  liftIO $ print fullPath
-  liftIO $ createDirectory $ fileBase ++ fullPath
+doMkCol urlPath = liftIO $ createDirectory =<< getSafePath urlPath
 
 doPropFind::[String]->Element->Handler [PropResults]
 doPropFind urlPath doc = do
   --TODO - check that the xml path element names are all correct....
   let propNames = [qName $ elName x | Elem x <- concat $ map elContent $ [x | Elem x <- elContent doc]]
-  liftIO $ putStrLn $ "In doPropFind: " ++ show propNames
+
   let relPath = concat $ map ("/" ++) urlPath
 
   let fullPath=fileBase++relPath
-  liftIO $ putStrLn $ "relPath: " ++ relPath
-  liftIO $ putStrLn $ "fullPath: " ++ fullPath
+
+  -- partial safety check, see notes below....
+  when ("/../" `isInfixOf` fullPath) $ error "Invalid path"
+
   isDir <- liftIO $ doesDirectoryExist fullPath
   isFile <- liftIO $ doesFileExist fullPath
 
@@ -127,3 +91,42 @@ doPropFind urlPath doc = do
 
   for files $ 
       liftIO . getPropResults propNames
+
+
+
+
+
+
+
+-- this function gets the local filepath
+-- It is important that the path can not go outside of the webdav folder, so we have to make sure
+-- that no double dot paths are in the filepath.
+-- Honestly, it looks like servant doesn't allow this anyway, so this check may never trigger.
+-- Also, the better way to do this is to normalize the path (remove dots, double slashes, etc),
+-- then verify that it fall in the path.  Unfortunately I couldn't find a good pre-written
+-- normalization function, so I will just do a simpler comparison for now.
+getSafePath::[String]->IO FilePath
+getSafePath urlPath = do
+  let fullPath = concat $ map ("/" ++) urlPath
+  if "/../" `isInfixOf` fullPath
+    then error "Invalid path"
+    else return $ fileBase ++ fullPath
+
+-- This is a variant of getSafePath for paths given in the headers.
+-- I don't think servant checks for dot paths in the header values, so this check is
+-- much more important here.
+-- This should probably be thought through a bit more.
+getSafePath2::String->IO FilePath
+getSafePath2 urlPath = do
+  let urlPath' = Char8.unpack (urlDecode False (Char8.pack urlPath))
+
+  let relativePath =
+        if webBase `isPrefixOf` urlPath'
+        then drop (length webBase) urlPath'
+        else error "destination is not on this webdav server"
+
+  let fullPath = fileBase ++ relativePath
+
+  if "/../" `isInfixOf` fullPath
+    then error "Invalid path"
+    else return fullPath
