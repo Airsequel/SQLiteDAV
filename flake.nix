a41eba11f3544c7690e5b2a0028460bbfe7bf878
Adrian Sieber <mail@adriansieber.com>
2026-04-18 15:47:59 +0000

    Update dependencies, manage system dependencies with Nix Flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bash
+            coreutils # cat
+            gnumake
+            haskell.compiler.ghc910
+            haskellPackages.cabal-fmt
+            haskellPackages.cabal-install
+            haskellPackages.fourmolu
+            (pkgs.haskell-language-server.override {
+              supportedGhcVersions = [ "9103" ];
+            })
+            haskellPackages.hlint
+            haskellPackages.stack
+          ];
+        };
+        formatter = pkgs.nixfmt-tree; # Format this file with `nix fmt`
+      }
+    );
+}
